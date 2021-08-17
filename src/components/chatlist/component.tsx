ac01a2c3f627b4c9c43fbd24f1582e64eb789245
Shivashankar-741 <shiva1894300@gmail.com>
2021-08-17 08:30:16 +0530

    feat: newjoined and chatlist card has implemented

--- src/components/chatlist/component.tsx
+++ src/components/chatlist/component.tsx
@@ -1,6 +1,61 @@
 import React from 'react';
 import './styles.css';
+import img1 from '../../assets/images/2.png';
+import img2 from '../../assets/images/Ellipse 5.png';
+import img3 from '../../assets/images/Ellipse 33.png';
 
 export const Chatlist = () => {
-  return <div className="chatlist"></div>;
+  return (
+    <div className="chatlist">
+      <h1 className="chatlist__title">Chat list</h1>
+      <div className="chatlist__lists">
+        <div className="chatlist__list">
+          <div className="chatlist__list--left">
+            <div className="chatlist__list--left-profile">
+              <img className="chatlist__list--left-profile-img" src={img1} alt="profile" />
+            </div>
+            <div className="chatlist__list--left-profile-details">
+              <h1 className="chatlist__list--left-profile-details-name">Raghav</h1>
+              <h1 className="chatlist__list--left-profile-details-lastmsg">Dinner?</h1>
+            </div>
+          </div>
+          <div className="chatlist__list--right">
+            <h5 className="chatlist__list--right-lastmsgtime">Today, 7.38pm</h5>
+          </div>
+        </div>
+      </div>
+      <div className="chatlist__lists">
+        <div className="chatlist__list">
+          <div className="chatlist__list--left">
+            <div className="chatlist__list--left-profile">
+              <img className="chatlist__list--left-profile-img" src={img2} alt="profile" />
+            </div>
+            <div className="chatlist__list--left-profile-details">
+              <h1 className="chatlist__list--left-profile-details-name">Swathi</h1>
+              <h1 className="chatlist__list--left-profile-details-lastmsg">Hi there!</h1>
+            </div>
+          </div>
+          <div className="chatlist__list--right">
+            <h5 className="chatlist__list--right-lastmsgtime">yesterday, 6.51pm</h5>
+          </div>
+        </div>
+      </div>
+      <div className="chatlist__lists">
+        <div className="chatlist__list">
+          <div className="chatlist__list--left">
+            <div className="chatlist__list--left-profile">
+              <img className="chatlist__list--left-profile-img" src={img3} alt="profile" />
+            </div>
+            <div className="chatlist__list--left-profile-details">
+              <h1 className="chatlist__list--left-profile-details-name">Teju</h1>
+              <h1 className="chatlist__list--left-profile-details-lastmsg">Hey</h1>
+            </div>
+          </div>
+          <div className="chatlist__list--right">
+            <h5 className="chatlist__list--right-lastmsgtime">Today, 9.41pm</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/newlyjoined/component.tsx
+++ src/components/newlyjoined/component.tsx
@@ -2,11 +2,12 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import './styles.css';
 import img1 from '../../assets/images/Ellipse 2.png';
+import img2 from '../../assets/images/Ellipse 3.png';
 
 export const Newlyjoined = () => {
   return (
     <div className="newlyjoined">
-      <h1 className="newlyjoined__title">Groups</h1>
+      <h1 className="newlyjoined__title">Newly joined</h1>
       <div className="newlyJoined__lists">
         <div className="newlyJoined__list">
           <div className="newlyJoined__list--left">
@@ -22,6 +23,34 @@ export const Newlyjoined = () => {
             <h5 className="newlyJoined__list--right-lastmsgtime">Today, 5.35pm</h5>
           </div>
         </div>
+        <div className="newlyJoined__list">
+          <div className="newlyJoined__list--left">
+            <div className="newlyJoined__list--left-profile">
+              <img className="newlyJoined__list--left-profile-img" src={img2} alt="profile" />
+            </div>
+            <div className="newlyJoined__list--left-profile-details">
+              <h1 className="newlyJoined__list--left-profile-details-name">Swathi</h1>
+              <h1 className="newlyJoined__list--left-profile-details-lastmsg">Hi there!</h1>
+            </div>
+          </div>
+          <div className="newlyJoined__list--right">
+            <h5 className="newlyJoined__list--right-lastmsgtime">Today, 2.15pm</h5>
+          </div>
+        </div>
+        {/* <div className="newlyJoined__list">
+          <div className="newlyJoined__list--left">
+            <div className="newlyJoined__list--left-profile">
+              <img className="newlyJoined__list--left-profile-img" src={img1} alt="profile" />
+            </div>
+            <div className="newlyJoined__list--left-profile-details">
+              <h1 className="newlyJoined__list--left-profile-details-name">Swathi</h1>
+              <h1 className="newlyJoined__list--left-profile-details-lastmsg">Hi there!</h1>
+            </div>
+          </div>
+          <div className="newlyJoined__list--right">
+            <h5 className="newlyJoined__list--right-lastmsgtime">Today, 2.15pm</h5>
+          </div>
+        </div> */}
       </div>
     </div>
   );
