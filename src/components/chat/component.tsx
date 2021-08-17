5c1fbdf789a2199d14f4074290b7dfd6413f0e79
Shivashankar-741 <shiva1894300@gmail.com>
2021-08-17 12:04:47 +0530

    feat: chat screen profile has created

--- src/components/chat/component.tsx
+++ src/components/chat/component.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './styles.css';
+import img2 from '../../assets/images/Ellipse 33.png';
+import { EllipsisIcon } from '../../icons';
 
 export const Chat = () => {
-  return <div className="chat"></div>;
+  return (
+    <div className="chat">
+      <div className="chat__profile">
+        <div className="chat__profile__left">
+          <img className="chat__profile__left-img" src={img2} alt="profile" />
+          <h1 className="chat__profile__left-name">Swathi</h1>
+        </div>
+        <div className="chat__profile__right">
+          <div className="chat__profile__right-ellipsis">
+            <EllipsisIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,3 +2,4 @@ export { ChatIcon } from './chat.icon';
 export { NotificationIcon } from './notification.icon';
 export { SettingsIcon } from './settings.icon';
 export { LogoutIcon } from './logout.icon';
+export { EllipsisIcon } from './ellipsis.icon';
