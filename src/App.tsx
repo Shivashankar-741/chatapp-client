5cf35b1aff54448ed6d1b7856e056f2e48f027bb
Shivashankar-741 <shiva1894300@gmail.com>
2021-08-28 08:59:40 +0530

    fix: auth when localstorage gets null bug has fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { Grid } from "@material-ui/core";
+import { useState } from "react";
 import "./App.css";
 import { Sidebar, Searchbar, Newlyjoined, Chatlist } from "./components";
 import Auth from "./components/auth/component";
 import { Chat } from "./components/chat/component";
 
 function App() {
+  const [user, setUser] = useState(localStorage.getItem("chatapp"));
+  // console.log(user);
+
+  let parsedUser;
+  if (user) {
+    parsedUser = JSON.parse(user);
+  }
+
+  // console.log(parsedUser);
+  // console.log(parsedUser?.status);
+
   return (
     <div className="App">
-      {false ? (
+      {parsedUser ? (
         <Grid container>
           <Grid item xs={1}>
-            <Sidebar />
+            <Sidebar setUser={setUser} photo={parsedUser?.data?.user?.photo} />
           </Grid>
           <Grid item xs={12} sm={12} md={5} lg={5} xl={5}>
             <Searchbar />
