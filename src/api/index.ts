45d428e3ef91b59f19b8aac629a2176e9771faf1
Shivashankar-741 <shiva1894300@gmail.com>
2021-08-28 09:01:25 +0530

    feat:  api's structured folder

---
+++ src/api/index.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "http://localhost:6400/api/v1" });
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("chatapp")) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("chatapp") || "").token}`;
+  }
+  return req;
+});
+
+interface IFormData {
+  name?: string;
+  email: string;
+  password: string;
+  confirmPassword?: string;
+  photo?: string;
+}
+
+export const signUp = (formData: IFormData) => API.post(`/users/signup`, formData);
+export const LogIn = (formData: IFormData) => API.post(`/users/login`, formData);
+
+export const getAllUsers = () => API.get(`/users`);
